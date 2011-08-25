de4fc41cd6786fda0f1f89281303cbafde96653c
Zainan Victor Zhou <xinbenlv@gmail.com>
2011-08-25 11:38:08 -0400

    Before Generated Layout/Views Porjects

---
+++ generator.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Hello Layouts
+for var in LinearLayout RelativeLayout TableLayout TabLayout DatePicker TimePicker FormStuff Spinner AutoComplete Gallery GoolgeMapView WebView
+do
+android create project --target 1  --path ./Hello{$var} --activity Hello$var --package com.example.Hello{$var}
+done
+
+

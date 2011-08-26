ea2817417511ab9185ccc3dd07cb8ba00343a3c1
Zainan Victor Zhou <xinbenlv@gmail.com>
2011-08-26 17:03:27 -0400

    List View part1:hardcoded

--- HelloViews/generator.sh
+++ HelloViews/generator.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Hello Layouts
-for var in GridView LinearLayout RelativeLayout TableLayout TabLayout DatePicker TimePicker FormStuff Spinner AutoComplete Gallery GoolgeMapView WebView
-#for var in GridView 
+
+for var in GridView ListView LinearLayout RelativeLayout TableLayout TabLayout DatePicker TimePicker FormStuff Spinner AutoComplete Gallery GoolgeMapView WebView
 do
 android create project --target 1  --path ./Hello$var --activity Hello$var --package com.example.Hello$var
 done

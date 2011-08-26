d30eabe3d9dfb9b7447b8c3f1cfd70d0eed77a05
Zainan Victor Zhou <xinbenlv@gmail.com>
2011-08-26 15:12:17 -0400

    GridView

--- HelloViews/generator.sh
+++ HelloViews/generator.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #Hello Layouts
-for var in LinearLayout RelativeLayout TableLayout TabLayout DatePicker TimePicker FormStuff Spinner AutoComplete Gallery GoolgeMapView WebView
+for var in GridView LinearLayout RelativeLayout TableLayout TabLayout DatePicker TimePicker FormStuff Spinner AutoComplete Gallery GoolgeMapView WebView
+#for var in GridView 
 do
 android create project --target 1  --path ./Hello$var --activity Hello$var --package com.example.Hello$var
 done

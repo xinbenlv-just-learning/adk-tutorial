6f5d20fbfb65458738541176abe197f052997ee8
Zainan Victor Zhou <xinbenlv@gmail.com>
2011-08-29 09:38:49 -0400

    AutoComplete: Finished

--- HelloViews/HelloAutoComplete/src/com/example/HelloAutoComplete/HelloAutoComplete.java
+++ HelloViews/HelloAutoComplete/src/com/example/HelloAutoComplete/HelloAutoComplete.java
@@ -2,14 +2,22 @@ package com.example.HelloAutoComplete;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class HelloAutoComplete extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_country);
+        String[] countries = getResources().getStringArray(R.array.countries_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, countries);
+        
+        textView.setAdapter(adapter);
     }
+
 }
